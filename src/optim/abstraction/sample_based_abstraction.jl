4b21ec0c458d1a99c38d888fa12b8a74133e411b
ajans123 <adrien.janssens@student.uclouvain.be>
2025-01-08 16:08:29 +0100

    reach to safe (simple pendulum)

--- src/optim/abstraction/sample_based_abstraction.jl
+++ src/optim/abstraction/sample_based_abstraction.jl
@@ -296,6 +296,8 @@ function compute_controller_safe!(contr, autom, initlist, safelist)
             end
         end
     end
+    l = length(safelist)
+    println("Number of safe cells: $l")
     if ⊆(initlist, safeset)
         println("\ncompute_controller_safe! terminated with success")
     else
