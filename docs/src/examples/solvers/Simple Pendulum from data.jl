e631d466eb715da83305241f1cc962c4e93db58f
ajans123 <adrien.janssens@student.uclouvain.be>
2025-01-14 10:09:23 +0100

    nothing important

--- docs/src/examples/solvers/Simple Pendulum from data.jl
+++ docs/src/examples/solvers/Simple Pendulum from data.jl
@@ -14,7 +14,7 @@ include(joinpath(dirname(dirname(pathof(Dionysos))), "problems/simple_pendulum.j
 concrete_problem = Pendulum.problem(; approx_mode = "growth")
 concrete_system = concrete_problem.system
 x0 = SVector(0.0, 0.0)
-hx = SVector(0.1, 0.1)
+hx = SVector(0.2, 0.2)
 state_grid = DO.GridFree(x0, hx)
 u0 = SVector(0.0);
 h = SVector(0.3);
